1ae344180edb552842458d1f761f2bee22391bb5
15847 <1357783650@qq.com>
2019-01-24 16:17:20 +0800

    商品列表及详情

--- supermarket/orders/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from goods.views import main, index
-
-urlpatterns = [
-    url(r'^main/$',main,name='订单提交'),
-    url(r'^index/$',index,name='订单信息')
-]
--- supermarket/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from user.views import RegisterView, LoginView, userinfo
-
-urlpatterns = [
-    url(r'^userinfo/$',userinfo,name='用户详情'),
-    url(r'^reg$',RegisterView.as_view(),name='注册'),
-    url(r'^login$',LoginView.as_view(),name='登录')
-]
